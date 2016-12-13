3ba1cdbed6a04834fcb9fc9a0c305f07b83da546
klaus31 <danieloltmanns@gmx.de>
2016-12-13 15:38:33 +0100

    include phaser from node modules

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ var app = express();
 var port = 8080;
 
 app.use(express.static(__dirname + '/www/.'));
+app.use(express.static(__dirname + '/node_modules/.'));
+
 
 app.listen(port, function() {
   console.info('please visit: http://localhost:' + port);
