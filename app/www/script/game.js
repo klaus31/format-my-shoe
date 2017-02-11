ec280f3abacf26aedb3af4dc36e47a08542c03c2
klaus31 <danieloltmanns@gmx.de>
2017-02-11 14:26:23 +0100

    init labyrinth

--- app/www/script/game.js
+++ app/www/script/game.js
@@ -1,47 +1,25 @@
 let Game = function() {
 
-  const hero = new Hero();
-  const rank = new Rank();
-  const ground = new Ground();
-  const ledge = new Ledge();
-  const beer = new Beer();
+  const ID_WALL = 2;
+  const labyrinth = new Labyrinth();
 
-  let collisionGroup;
+  let map;
+  let layer;
   let cursors;
 
   this.preload = function() {
     cursors = game.input.keyboard.createCursorKeys();
-
-    hero.load(game);
-    rank.load(game);
-    ground.load(game);
-    ledge.load(game);
-    beer.load(game);
+    labyrinth.preload();
   }
 
   this.create = function() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.stage.backgroundColor = "#B0D0FF";
-
-    collisionGroup = game.add.group();
-    collisionGroup.enableBody = true;
-
-    rank.addToGroup(collisionGroup, game.world.width - 64, game.world.height - 160, 2);
-    rank.addToGroup(collisionGroup, 0, game.world.height - 160, 3);
-
-    ground.addToGroup(collisionGroup, 0, game.world.height - 64);
-    ground.addToGroup(collisionGroup, 425, game.world.height - 64);
-
-    ledge.addToGroup(collisionGroup, 400, 340);
-    ledge.addToGroup(collisionGroup, 500, 100);
-    ledge.addToGroup(collisionGroup, -150, 200);
-
-    beer.addToGroup(collisionGroup, game.world.width - 450, game.world.height - 64 - 32);
-
-    hero.addToGame(game, game.world.width / 2, game.world.height - 150);
+    labyrinth.create();
+    labyrinth.onWallHit(function() {
+      window.alert('kaputt');
+    });
   }
 
   this.update = function() {
-    hero.update(game, collisionGroup, cursors);
+    // labyrinth.update();
   }
 }
