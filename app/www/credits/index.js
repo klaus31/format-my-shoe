8fbe8d078446444382003b65edd8e29b5d0a2ba5
klaus31 <danieloltmanns@gmx.de>
2017-02-20 20:04:07 +0100

    credits

--- app/www/credits/index.js
+++ app/www/credits/index.js
@@ -1,27 +1,23 @@
 let Credits = function() {
 
-  // TODO
-  const CREDIT_TEXT = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'.repeat(20);
+  // TODO credits text
+  const CREDIT_TEXT = 'Credits: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'.repeat(20);
   const FONT_STYLE = {
     fontSize: '10px',
-    fill: '#000',
-    font: 'Courier'
-  };
-  const FONT_STYLE_H1 = {
-    fontSize: '16px',
     fill: '#000',
     font: 'Courier'
   };
 
   this.preload = function() {
     game.stage.backgroundColor = '#FFF';
+    game.load.script('webfont', 'global/webfont.js');
   }
 
   this.create = function() {
-    let y = 10;
-    game.add.text(20, y, 'FINISHED GAME!', FONT_STYLE_H1);
-    y += 20;
-    game.add.text(20, y += 10, 'Credits:', FONT_STYLE);
+    let y = 40;
+    let fontCtrl = new FontCtrl();
+    fontCtrl.addText(20,y,'FINISHED GAME!');
+    y += 40;
     let text = CREDIT_TEXT;
     while (text.length > 0) {
       let line = text.slice(0, 80);
