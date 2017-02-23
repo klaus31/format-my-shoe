0c615e06b6227e4ea64e9314896950b9322a97d5
klaus31 <danieloltmanns@gmx.de>
2017-02-23 18:53:28 +0100

    do not stop driving

--- app/www/game/drive.js
+++ app/www/game/drive.js
@@ -5,8 +5,7 @@ let Drive = function(speed) {
   let x;
   let y;
   let firstMoveMade = false;
-  let wasDown = false;
-  let stateBeforePointerDown;
+  let prevX = false;
 
   this.stop = function() {
     x = 0;
@@ -24,23 +23,17 @@ let Drive = function(speed) {
   }
   this.update = function(sprite) {
     if (game.input.activePointer.isDown) {
-      wasDown = true;
-      ME.stop();
-      if (!stateBeforePointerDown) stateBeforePointerDown = {
-        angle: sprite.angle,
-        x: game.input.worldX
-      };
-      let newAngle = stateBeforePointerDown.angle + game.input.worldX - stateBeforePointerDown.x;
-      if (Math.abs(newAngle % 90) < ANGLE_SNAP_INTO_PLACE || Math.abs(newAngle % 90) > 90 - ANGLE_SNAP_INTO_PLACE) {
-        newAngle = Math.round(newAngle / 90) * 90;
+      if(prevX) {
+        firstMoveMade = true;
+        let newAngle = sprite.angle + game.input.x - prevX;
+        sprite.angle = newAngle;
+        x = speed * Math.cos(sprite.angle * Math.PI / 180);
+        y = speed * Math.sin(sprite.angle * Math.PI / 180);
       }
-      sprite.angle = newAngle;
-    };
-    if (wasDown && game.input.activePointer.isUp) {
-      firstMoveMade = true;
-      stateBeforePointerDown = false;
-      x = speed * Math.cos(sprite.angle * Math.PI / 180);
-      y = speed * Math.sin(sprite.angle * Math.PI / 180);
-    };
+      prevX = game.input.x;
+    }
+    if (game.input.activePointer.isUp) {
+      prevX = false;
+    }
   }
 }
